42858be6c14b2191c67e8ef7ca1efdc4e0d4f515
Allan Letona <aletonam@miumg.edu.gt>
2019-11-05 23:12:13 -0600

    Actualizacion de Mantenimientos

--- Finanzas/frm_cuentasContables.Designer.cs
+++ Finanzas/frm_cuentasContables.Designer.cs
@@ -28,33 +28,277 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.navegador1 = new CapaDeDiseno.Navegador();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.btn_modificar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_cancelar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgv_cuentasContables = new System.Windows.Forms.DataGridView();
+            this.txt_CodigoCuenta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbo_tipoCuenta = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_nombreCuenta = new System.Windows.Forms.TextBox();
+            this.txt_estado = new System.Windows.Forms.TextBox();
+            this.txt_descripcionCuenta = new System.Windows.Forms.RichTextBox();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_cuentasContables)).BeginInit();
             this.SuspendLayout();
             // 
-            // navegador1
+            // btn_ingresar
             // 
-            this.navegador1.BackColor = System.Drawing.Color.White;
-            this.navegador1.Location = new System.Drawing.Point(-1, -1);
-            this.navegador1.Name = "navegador1";
-            this.navegador1.Size = new System.Drawing.Size(1178, 657);
-            this.navegador1.TabIndex = 0;
+            this.btn_ingresar.Location = new System.Drawing.Point(206, 22);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(82, 33);
+            this.btn_ingresar.TabIndex = 0;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
+            // 
+            // btn_modificar
+            // 
+            this.btn_modificar.Location = new System.Drawing.Point(303, 22);
+            this.btn_modificar.Name = "btn_modificar";
+            this.btn_modificar.Size = new System.Drawing.Size(82, 33);
+            this.btn_modificar.TabIndex = 1;
+            this.btn_modificar.Text = "Modificar";
+            this.btn_modificar.UseVisualStyleBackColor = true;
+            this.btn_modificar.Click += new System.EventHandler(this.btn_modificar_Click);
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Location = new System.Drawing.Point(402, 22);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(77, 33);
+            this.btn_guardar.TabIndex = 2;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // btn_cancelar
+            // 
+            this.btn_cancelar.Location = new System.Drawing.Point(599, 22);
+            this.btn_cancelar.Name = "btn_cancelar";
+            this.btn_cancelar.Size = new System.Drawing.Size(86, 33);
+            this.btn_cancelar.TabIndex = 3;
+            this.btn_cancelar.Text = "Cancelar";
+            this.btn_cancelar.UseVisualStyleBackColor = true;
+            this.btn_cancelar.Click += new System.EventHandler(this.btn_cancelar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(494, 22);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(90, 33);
+            this.btn_eliminar.TabIndex = 4;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(2, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(950, 521);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgv_cuentasContables);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(942, 491);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Registros";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btn_salir);
+            this.tabPage2.Controls.Add(this.txt_descripcionCuenta);
+            this.tabPage2.Controls.Add(this.txt_estado);
+            this.tabPage2.Controls.Add(this.txt_nombreCuenta);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.cbo_tipoCuenta);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.txt_CodigoCuenta);
+            this.tabPage2.Controls.Add(this.btn_eliminar);
+            this.tabPage2.Controls.Add(this.btn_ingresar);
+            this.tabPage2.Controls.Add(this.btn_cancelar);
+            this.tabPage2.Controls.Add(this.btn_modificar);
+            this.tabPage2.Controls.Add(this.btn_guardar);
+            this.tabPage2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(942, 491);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Mantenimiento";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgv_cuentasContables
+            // 
+            this.dgv_cuentasContables.AllowUserToAddRows = false;
+            this.dgv_cuentasContables.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_cuentasContables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_cuentasContables.Location = new System.Drawing.Point(0, 3);
+            this.dgv_cuentasContables.Name = "dgv_cuentasContables";
+            this.dgv_cuentasContables.ReadOnly = true;
+            this.dgv_cuentasContables.Size = new System.Drawing.Size(942, 489);
+            this.dgv_cuentasContables.TabIndex = 0;
+            this.dgv_cuentasContables.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_cuentasContables_CellClick);
+            this.dgv_cuentasContables.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_cuentasContables_CellDoubleClick);
+            // 
+            // txt_CodigoCuenta
+            // 
+            this.txt_CodigoCuenta.Location = new System.Drawing.Point(465, 157);
+            this.txt_CodigoCuenta.Name = "txt_CodigoCuenta";
+            this.txt_CodigoCuenta.Size = new System.Drawing.Size(193, 23);
+            this.txt_CodigoCuenta.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Codigo de Cuenta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tipo de Cuenta";
+            // 
+            // cbo_tipoCuenta
+            // 
+            this.cbo_tipoCuenta.FormattingEnabled = true;
+            this.cbo_tipoCuenta.Location = new System.Drawing.Point(465, 107);
+            this.cbo_tipoCuenta.Name = "cbo_tipoCuenta";
+            this.cbo_tipoCuenta.Size = new System.Drawing.Size(193, 25);
+            this.cbo_tipoCuenta.TabIndex = 8;
+            this.cbo_tipoCuenta.SelectedIndexChanged += new System.EventHandler(this.cbo_tipoCuenta_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nombre de Cuenta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(302, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Descripcion de Cuenta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(396, 390);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Estado";
+            // 
+            // txt_nombreCuenta
+            // 
+            this.txt_nombreCuenta.Location = new System.Drawing.Point(465, 208);
+            this.txt_nombreCuenta.Name = "txt_nombreCuenta";
+            this.txt_nombreCuenta.Size = new System.Drawing.Size(193, 23);
+            this.txt_nombreCuenta.TabIndex = 12;
+            // 
+            // txt_estado
+            // 
+            this.txt_estado.Location = new System.Drawing.Point(465, 384);
+            this.txt_estado.Name = "txt_estado";
+            this.txt_estado.Size = new System.Drawing.Size(100, 23);
+            this.txt_estado.TabIndex = 14;
+            // 
+            // txt_descripcionCuenta
+            // 
+            this.txt_descripcionCuenta.Location = new System.Drawing.Point(465, 253);
+            this.txt_descripcionCuenta.Name = "txt_descripcionCuenta";
+            this.txt_descripcionCuenta.Size = new System.Drawing.Size(252, 96);
+            this.txt_descripcionCuenta.TabIndex = 15;
+            this.txt_descripcionCuenta.Text = "";
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(701, 22);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(78, 33);
+            this.btn_salir.TabIndex = 16;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
             // 
             // frm_cuentasContables
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1175, 663);
-            this.Controls.Add(this.navegador1);
+            this.ClientSize = new System.Drawing.Size(949, 529);
+            this.Controls.Add(this.tabControl1);
             this.Name = "frm_cuentasContables";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cuentas Contables";
             this.Load += new System.EventHandler(this.frm_cuentasContables_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_cuentasContables)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private CapaDeDiseno.Navegador navegador1;
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.Button btn_modificar;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_cancelar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dgv_cuentasContables;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.RichTextBox txt_descripcionCuenta;
+        private System.Windows.Forms.TextBox txt_estado;
+        private System.Windows.Forms.TextBox txt_nombreCuenta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbo_tipoCuenta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_CodigoCuenta;
     }
 }
--- Finanzas/frm_cuentasContables.cs
+++ Finanzas/frm_cuentasContables.cs
@@ -7,34 +7,223 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaLogicaFinanzas;
 
 namespace Finanzas
 {
     public partial class frm_cuentasContables : Form
     {
-        string usuario = "";
+        int boton = 0;
+        logica logic = new logica();
+        
         public frm_cuentasContables(string user)
         {
             InitializeComponent();
+            llenarComboBox();
+           
 
-            usuario = user;
-            string[] alias = { "Codigo Cuenta", "Tipo de Cuenta", "Nombre", "Descripcion", "Estado" };
-            navegador1.asignarAlias(alias);
-            navegador1.asignarSalida(this);
-            navegador1.asignarColorFondo(Color.White);
-            navegador1.asignarColorFuente(Color.Black);
-            navegador1.asignarComboConTabla("tbl_tipoCuenta", "nombre_tipoCuenta",1);
-            navegador1.asignarAyuda("1");
-            navegador1.asignarTabla("tbl_cuentas");
-            navegador1.asignarNombreForm("Cuentas Contables");
+        }
+
+        void llenarComboBox()
+        {
+            DataTable llenarCombo = logic.consultaLogicaTipoCuentaContable();
+
+            foreach (DataRow row in llenarCombo.Rows)
+            {
+                cbo_tipoCuenta.Items.Add(row[0].ToString());
+            }
+        }
+
+        void limpiarForm()
+        {
+            txt_CodigoCuenta.Text = "";
+            txt_descripcionCuenta.Text = "";
+            txt_estado.Text = "";
+            txt_nombreCuenta.Text = "";
+            cbo_tipoCuenta.Text = "";
         }
 
         private void frm_cuentasContables_Load(object sender, EventArgs e)
         {
-            string aplicacionActiva = "1";
-            navegador1.ObtenerIdUsuario(usuario);
-            navegador1.botonesYPermisosInicial(usuario, aplicacionActiva);
-            navegador1.ObtenerIdAplicacion(aplicacionActiva);
+            dgv_cuentasContables.DataSource = logic.consultaLogicaCuentasContables().Tables[0];
+            cbo_tipoCuenta.Enabled = false;
+            txt_CodigoCuenta.Enabled = false;
+            txt_descripcionCuenta.Enabled = false;
+            txt_estado.Enabled = false;
+            txt_nombreCuenta.Enabled = false;
+        }
+
+        void bloquearBotones()
+        {
+            btn_ingresar.Enabled = false;
+            btn_modificar.Enabled = false;
+            btn_eliminar.Enabled = false;
+ 
+        }
+
+        void habilitarBotones()
+        {
+            btn_ingresar.Enabled = true;
+            btn_modificar.Enabled = true;
+            btn_eliminar.Enabled = true;
+        }
+
+        void habilitarCajasTexto()
+        {
+            cbo_tipoCuenta.Enabled = true;
+            txt_CodigoCuenta.Enabled = true;
+            txt_descripcionCuenta.Enabled = true;
+            txt_estado.Enabled = true;
+            txt_nombreCuenta.Enabled = true;
+
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
+        {
+            boton = 1;
+            bloquearBotones();
+            habilitarCajasTexto();
+            limpiarForm();
+            txt_CodigoCuenta.Enabled = false;
+            txt_estado.Text = "1";
+            txt_estado.Enabled = false;
+        }
+
+        private void btn_modificar_Click(object sender, EventArgs e)
+        {
+            boton = 2;
+            bloquearBotones();
+            habilitarCajasTexto();
+            txt_CodigoCuenta.Enabled = false;
+            cbo_tipoCuenta.Enabled = false;
+        }
+
+        private void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            habilitarBotones();
+            cbo_tipoCuenta.Enabled = false;
+            txt_CodigoCuenta.Enabled = false;
+            txt_descripcionCuenta.Enabled = false;
+            txt_estado.Enabled = false;
+            txt_nombreCuenta.Enabled = false;
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        int renglon;
+
+        private void dgv_cuentasContables_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            renglon = e.RowIndex;
+        }
+
+        private void dgv_cuentasContables_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tabPage2.Show();
+
+            string sCodigoCuenta;
+            string sTipoCuenta;
+            string sNombreCuenta;
+            string sDescripcionCuenta;
+            string sEstado;
+
+            sCodigoCuenta = dgv_cuentasContables.Rows[renglon].Cells[0].Value.ToString();
+            txt_CodigoCuenta.Text = sCodigoCuenta;
+
+            sTipoCuenta = dgv_cuentasContables.Rows[renglon].Cells[1].Value.ToString();
+            cbo_tipoCuenta.Text = sTipoCuenta;
+
+            sNombreCuenta = dgv_cuentasContables.Rows[renglon].Cells[2].Value.ToString();
+            txt_nombreCuenta.Text = sNombreCuenta;
+
+            sDescripcionCuenta = dgv_cuentasContables.Rows[renglon].Cells[3].Value.ToString();
+            txt_descripcionCuenta.Text = sDescripcionCuenta;
+
+            sEstado = dgv_cuentasContables.Rows[renglon].Cells[4].Value.ToString();
+            txt_estado.Text = sEstado;
+
+            tabControl1.SelectedTab = tabPage2;
+
+
+        }
+         
+
+        void deshabilitarForm()
+        {
+            cbo_tipoCuenta.Enabled = false;
+            txt_CodigoCuenta.Enabled = false;
+            txt_descripcionCuenta.Enabled = false;
+            txt_estado.Enabled = false;
+            txt_nombreCuenta.Enabled = false;
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+
+            string sTipoCuenta = cbo_tipoCuenta.SelectedItem.ToString();
+            string sNombreCuenta = txt_nombreCuenta.Text;
+            string sDescripcionCuenta = txt_descripcionCuenta.Text; 
+            string sEstado = txt_estado.Text;
+
+
+            if (boton == 1)
+            {
+               
+                try
+                {
+                    string sCodigoCuenta = txt_CodigoCuenta.Text;
+                    logic.consultaLogicaIngresoCuentasContables(sCodigoCuenta, sTipoCuenta, sNombreCuenta, sDescripcionCuenta, sEstado);
+                    MessageBox.Show("Registro Almacenado Exitosamente");
+                    limpiarForm();
+                    habilitarBotones();
+                    deshabilitarForm();
+                    dgv_cuentasContables.DataSource = logic.consultaLogicaCuentasContables().Tables[0];
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            else if(boton == 2)
+            {
+                string sCodigoCuenta = txt_CodigoCuenta.Text;
+                MessageBox.Show(sNombreCuenta);
+                MessageBox.Show(sDescripcionCuenta);
+                logic.consultaLogicaModificarCuenta(sCodigoCuenta, sNombreCuenta, sDescripcionCuenta, sEstado);
+                MessageBox.Show("Registro Modificado Exitosamente");
+                limpiarForm();
+                habilitarBotones();
+                deshabilitarForm();
+                dgv_cuentasContables.DataSource = logic.consultaLogicaCuentasContables().Tables[0];
+            }
+
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            
+        }
+        string codigoTipocuenta;
+        private void cbo_tipoCuenta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sTipoCuenta = cbo_tipoCuenta.SelectedItem.ToString();
+
+            codigoTipocuenta = logic.consultaLogicaCodigoDeTipoDeCuenta(sTipoCuenta);
+            DataTable maxCodigoCuentaContable = logic.consultaLogicaMaxCuentaContable(codigoTipocuenta);
+
+            foreach (DataRow row in maxCodigoCuentaContable.Rows)
+            {
+
+                string maxCodigo = row[0].ToString();
+                string maxcodigo2 = maxCodigo.Substring(maxCodigo.Length - 1, 1);
+                int maximo = Convert.ToInt32(maxcodigo2);
+                maximo++;
+                string siguienteCodigo = codigoTipocuenta + "." + maximo;
+                txt_CodigoCuenta.Text = siguienteCodigo;
+            }
         }
     }
 }
