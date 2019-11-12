b668e19680ea43a384f7c7abfcdd523c51b979c1
keyshardk <dgomezg5@miumg.edu.gt>
2019-11-12 00:24:24 -0600

    Actualizacion Creacion Presupuesto

--- Finanzas/Frm_CreacionPresupuesto.Designer.cs
+++ Finanzas/Frm_CreacionPresupuesto.Designer.cs
@@ -48,6 +48,7 @@
             this.ClientSize = new System.Drawing.Size(1166, 672);
             this.Controls.Add(this.navegador1);
             this.Name = "Frm_CreacionPresupuesto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Frm_CreacionPresupuesto";
             this.Load += new System.EventHandler(this.Frm_CreacionPresupuesto_Load);
             this.ResumeLayout(false);
